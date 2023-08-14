aed489b52df67ae21efbb9fb2d4c499f066219ae
antialone <antialonedn1@gmail.com>
2023-08-14 18:53:24 +0700

    Update index.jsx
    update

--- src/components/Home/ChampionInfo/index.jsx
+++ src/components/Home/ChampionInfo/index.jsx
@@ -31,13 +31,15 @@ function ChampionInfo() {
   // use championData.${chamid}.data
   // liệu có thể truyền cả hàm useEffect vào giá trị của championData không
   return (
-    <div className="all-info flex flex-col justify-center items-center w-[95%] lg:w-[80%] xl:w-[70%] bg-slate-800 ">
+    <div className="all-info flex flex-col justify-center items-center w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px]  bg-slate-800 ">
       {!championData ? (
         <>Loadding</>
       ) : (
-        <div className="flex flex-col items-center">
+        <div className=" flex flex-col items-center justify-center overflow-hidden visible">
           <div className="header-info h-[100px] w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1080px] 2xl:w-[1440px] bg-slate-300 flex mx-auto justify-center items-end pb-[10px] ">
-            <p className="text-[40px] font-bold tracking-widest ">{championData[id].id}</p>
+            <p className="text-[40px] font-bold tracking-widest ">
+              {championData[id].id}
+            </p>
           </div>
           <Link
             to="/"
@@ -48,7 +50,7 @@ function ChampionInfo() {
           >
             Back to Home
           </Link>
-
+{/* ---------------------------------InFo------------------------------ */}
           <div className="container-info mx-auto flex items-center flex-col w-[1024px] text-white">
             <img
               className="cham-bg w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] xl:h-[620px] mb-[30px] "
@@ -61,7 +63,9 @@ function ChampionInfo() {
             <div className="lore-text w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] flex justify-center items-center h-[40px] bg-slate-500 mb-[30px] ">
               <p>LORE</p>
             </div>
-            <div className="cham-lore w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] mb-[30px]">{championData[id].lore}</div>
+            <div className="cham-lore w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] mb-[30px]">
+              {championData[id].lore}
+            </div>
             <div className="skill-box w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] ">
               <div className="skill-text w-full flex justify-center items-center h-[40px] bg-slate-500 mb-[30px] ">
                 <p>SKILL</p>
@@ -71,19 +75,21 @@ function ChampionInfo() {
                 spells={championData[id].spells}
               />
             </div>
-            <div className="skin-text  flex justify-center items-center h-[40px] w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] bg-slate-500 mb-[30px] "><p>SKIN</p></div>
+            <div className="skin-text  flex justify-center items-center h-[40px] w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] bg-slate-500 mb-[30px] ">
+              <p>SKIN</p>
+            </div>
             <div>
-              <div className="list-skins flex  mb-[30px] w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px]">
+              <div className="list-skins flex flex-col  mb-[30px] w-[480px] sm:w-[600px] md:w-[720px] xl:w-[1024px] h-[180px] flex-wrap overflow-auto">
                 {championData[id].skins.map((data, index) => {
                   return (
-                    <div className="skin-container" key={data.num}>
+                    <div className="skin-container relative h-[150px]  " key={data.num}>
                       <img
-                        className="skins"
+                        className="skins h-[150px]"
                         src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_${data.num}.jpg`}
                         alt={data.name}
                         onClick={() => handleSkinClick(index)}
                       />
-                      <div className="overlay-text">{data.name}</div>
+                      <div className="overlay-text absolute bottom-[10px] bg-black  ml-2 p-1 rounded-md">{data.name}</div>
                     </div>
                   );
                 })}
